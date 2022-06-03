1067f5badb5b157f5305f9e5804dc93dce2c2ad7
PrasanthGondi <gprasanthchowdary@gmail.com>
2022-06-03 12:27:41 +0000

    Adde

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const path = require("path");
+const app = express();
+const dbpath = path.join(__dirname, "app.db");
+app.use(express.json());
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.post("/post", async (request, response) => {
+  const { inputValue } = request.body;
+  console.log(inputValue);
+  const query1 = `INSERT INTO emoji(value) values ('${inputValue}')`;
+  const dbQuery1 = await db.run(query1);
+  response.send(request.body);
+});
+
+app.get("/list", async (request, response) => {
+  const query2 = `SELECT value FROM emoji`;
+  const dbquery2 = await db.all(query2);
+  response.send(dbquery2);
+  console.log(dbquery2);
+});
+
+app.delete("/", async (request, response) => {
+  const query1 = "DELETE FROM emoji";
+  const dbQuery1 = await db.run(query1);
+});
+
+module.exports = app;
